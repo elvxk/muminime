5aa6541a2ac768c9c2dc12f47ebc5e5045a0954f
elvxk <pradiptaramadan9c@gmail.com>
2024-03-01 00:47:34 +0700

    🖼 Add Sharp

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -3,7 +3,7 @@ import rateLimit from "axios-rate-limit";
 
 const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 const api = rateLimit(axios.create(), {
-  maxRequests: 3, // Jumlah maksimum permintaan
+  maxRequests: 1, // Jumlah maksimum permintaan
   perMilliseconds: 1000, // Periode waktu dalam milidetik (misalnya, 1000 ms = 1 detik)
 });
 
@@ -14,7 +14,13 @@ export const apiHit = (url: string) => {
       return res.data;
     })
     .catch((e) => {
-      console.error(e);
+      if (e.response && e.response.status === 429) {
+        console.error(
+          "You have reached your request limit. Please try again later.",
+        );
+      } else {
+        console.error("Error:", e.message);
+      }
     });
   return res;
 };
@@ -25,7 +31,13 @@ export const apiHitNest = (url: string) => {
       return res.data.flatMap((mumi: any) => mumi.entry);
     })
     .catch((e) => {
-      console.error(e);
+      if (e.response && e.response.status === 429) {
+        console.error(
+          "You have reached your request limit. Please try again later.",
+        );
+      } else {
+        console.error("Error:", e.message);
+      }
     });
   return res;
 };
