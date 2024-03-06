b03486928bf9e67415f5042a856c5141c31e1ccc
elvxk <pradiptaramadan9c@gmail.com>
2024-03-06 15:31:17 +0700

    🍭 Add Loading

--- src/components/pages/CatHome.tsx
+++ src/components/pages/CatHome.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
+import Loading from "@/app/loading";
 import SearchBox from "@/components/SearchBox";
 import SwiperCard from "@/components/utils/SwiperCard";
 import { apiHit, apiHitNest } from "@/libs/api";
@@ -16,10 +17,12 @@ interface ICatHome {
 const CatHome: React.FC<ICatHome> = ({ apiTop, apiRec, category }) => {
   const [topList, setTopList] = useState([]);
   const [recList, setRecList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true);
         const response1 = await apiHit(apiTop);
         const data1 = await response1;
         setTopList(data1.data);
@@ -28,6 +31,7 @@ const CatHome: React.FC<ICatHome> = ({ apiTop, apiRec, category }) => {
         let data2 = await response2;
         data2 = { data: data2.sort(() => 0.5 - Math.random()).slice(0, 10) };
         setRecList(data2.data);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -42,47 +46,53 @@ const CatHome: React.FC<ICatHome> = ({ apiTop, apiRec, category }) => {
   if (category === "manga") manga = true;
 
   return (
-    <div className="container mx-auto px-4 pt-[10vh]">
-      {/* Search Box */}
-      <div className="flex mb-8 justify-center flex-col items-center">
-        <h1 className="-mb-4 text-lg font-bold text-primary uppercase">
-          Search {category}
-        </h1>
-        <SearchBox panime={anime} pmanga={manga} />
-      </div>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="container mx-auto px-4 pt-[10vh]">
+          {/* Search Box */}
+          <div className="flex mb-8 justify-center flex-col items-center">
+            <h1 className="-mb-4 text-lg font-bold text-primary uppercase">
+              Search {category}
+            </h1>
+            <SearchBox panime={anime} pmanga={manga} />
+          </div>
 
-      {/* Top Anime */}
-      <div className="flex justify-between items-center">
-        <h1 className="page text-xl font-bold text-primary uppercase">
-          TOP {category}
-        </h1>
-        <Link
-          href={`/${category}/top`}
-          className="hover:scale-105 transition-all text-xs text-accent-content flex items-center justify-center gap-2"
-        >
-          more <FaRegHandPointRight />
-        </Link>
-      </div>
-      <Suspense fallback={<p>Loading...</p>}>
-        <SwiperCard data={topList} type={category} />
-      </Suspense>
+          {/* Top Anime */}
+          <div className="flex justify-between items-center">
+            <h1 className="page text-xl font-bold text-primary uppercase">
+              TOP {category}
+            </h1>
+            <Link
+              href={`/${category}/top`}
+              className="hover:scale-105 transition-all text-xs text-accent-content flex items-center justify-center gap-2"
+            >
+              more <FaRegHandPointRight />
+            </Link>
+          </div>
+          <Suspense fallback={<p>Loading...</p>}>
+            <SwiperCard data={topList} type={category} />
+          </Suspense>
 
-      {/* Recommendation Anime */}
-      <div className="flex justify-between items-center mt-8 lg:mt-12">
-        <h1 className="page text-xl font-bold text-primary uppercase">
-          RECOMMENDATION {category}
-        </h1>
-        <Link
-          href={`/${category}/recommendations`}
-          className="hover:scale-105 transition-all text-xs text-accent-content flex items-center justify-center gap-2"
-        >
-          more <FaRegHandPointRight />
-        </Link>
-      </div>
-      <Suspense fallback={<p>Loading...</p>}>
-        <SwiperCard data={recList} type={category} />
-      </Suspense>
-    </div>
+          {/* Recommendation Anime */}
+          <div className="flex justify-between items-center mt-8 lg:mt-12">
+            <h1 className="page text-xl font-bold text-primary uppercase">
+              RECOMMENDATION {category}
+            </h1>
+            <Link
+              href={`/${category}/recommendations`}
+              className="hover:scale-105 transition-all text-xs text-accent-content flex items-center justify-center gap-2"
+            >
+              more <FaRegHandPointRight />
+            </Link>
+          </div>
+          <Suspense fallback={<p>Loading...</p>}>
+            <SwiperCard data={recList} type={category} />
+          </Suspense>
+        </div>
+      )}
+    </>
   );
 };
 export default CatHome;
