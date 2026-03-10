cda019df800bd61e89c8fc3dfdd2d2de6a475e2a
elvxk <pradiptaramadan9c@gmail.com>
2026-03-11 04:04:53 +0700

    fix(image): add image url to config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.myanimelist.net",
+        hostname: "myanimelist.net",
       },
       {
         protocol: "https",
